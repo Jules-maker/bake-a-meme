ac8322f895522c9c47e2a5c105dfb5ad000b78ee
selim27000 <selim27000@hotmail.com>
2023-10-03 11:07:36 +0200

    0.1.2 add import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Login from "./Login/+page.svelte";
   import HeaderNav from "./HeaderNav/+page.svelte";
   import Cookies from "./Cookies/+page.svelte"
+  import { isAuthenticated, username, logout } from '$lib/auth';
 
   </script>
   
