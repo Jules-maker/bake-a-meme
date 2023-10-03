a5968234d1b2953e62dc8bd6b237346642a3f4fa
selim27000 <selim27000@hotmail.com>
2023-10-03 10:45:47 +0200

    0.1.2 set roles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
+<<<<<<< Updated upstream
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+=======
+<script>
+  import Login from "./Login/+page.svelte";
+  import HeaderNav from "./HeaderNav/+page.svelte";
+  import Cookies from "./Cookies/+page.svelte"
+  import { isAuthenticated, username, logout } from '$lib/auth';
+
+
+  </script>
+  
+  <main>
+    <HeaderNav />
+      {#if $isAuthenticated}
+    <p>Bienvenue, {$username} !</p>
+    <button on:click={logout}>Se déconnecter</button>
+  {:else}
+    <p>Vous n'êtes pas connecté, visiteur</p>
+  {/if}
+<Cookies />
+  </main>
+  
+  <style>
+  </style>
+>>>>>>> Stashed changes
