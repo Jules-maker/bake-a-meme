fb3be6d4ac9a1535f9c68155d98dec822f59af59
selim27000 <selim27000@hotmail.com>
2023-10-04 10:31:58 +0200

    0.1.3 v1 feed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,55 @@
 <script>
-  import Login from "./Login/+page.svelte";
-  import HeaderNav from "./HeaderNav/+page.svelte";
-  import Cookies from "./Cookies/+page.svelte"
-  import { isAuthenticated, username, logout } from '$lib/auth';
 
-  </script>
-  
-  <main>
-    <HeaderNav />
-      {#if $isAuthenticated}
-    <p>Bienvenue, {$username} !</p>
-    <button on:click={logout}>Se déconnecter</button>
-  {:else}
-    <p>Vous n'êtes pas connecté, visiteur</p>
-  {/if}
-<Cookies />
-  </main>
-  
-  <style>
-  </style>
+  import Feed from '../lib/components/feed/feed.svelte';
+
+  let selectedImage = null;
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type.startsWith("image/")) {
+      selectedImage = file;
+    } else {
+      alert("Veuillez sélectionner un fichier image valide.");
+    }
+  };
+
+  const uploadImage = async () => {
+  if (selectedImage) {
+    const imagePayload = { url: selectedImage.name };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/images", {
+        method: "POST",
+        body: JSON.stringify(imagePayload),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+
+      if (response.ok) {
+        alert("Image envoyée avec succès !");
+      } else {
+        const data = await response.json();
+        if (data.error) {
+          alert(`Erreur API: ${data.error}`);
+        } else {
+          alert("Une erreur s'est produite lors de l'envoi de l'image.");
+        }
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête API :", error);
+      alert("Une erreur s'est produite lors de la requête API.");
+    }
+  } else {
+    alert("Veuillez sélectionner une image à envoyer.");
+  }
+};
+
+</script>
+
+<main>
+  <h1>Envoi d'image vers API</h1>
+  <input type="file" accept="image/*" on:change={handleImageChange} />
+  <button on:click={uploadImage}>Envoyer</button>
+  <Feed />
+</main>
